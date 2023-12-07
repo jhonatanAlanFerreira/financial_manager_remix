2d9c3b6df415c7b716e3fb074fe14f4483022d85
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2023-12-07 11:47:01 -0300

    improve mainSeed

--- app/data/seeders/mainSeed.tsx
+++ app/data/seeders/mainSeed.tsx
@@ -1,7 +1,11 @@
 import logOperationTypeSeed from "~/data/seeders/logOperationTypeSeed";
 import { prisma } from "~/data/database.server";
 
-Promise.all([logOperationTypeSeed()])
+const seeders = [
+  logOperationTypeSeed()
+];
+
+Promise.all(seeders)
   .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
     console.log(e);
