33d0f49a666d08620b10d906a53788daa954800c
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2023-12-13 11:53:31 -0300

    solve console warnings

--- app/routes/__app.expenses.tsx
+++ app/routes/__app.expenses.tsx
@@ -1,3 +1,3 @@
 export default function Expenses() {
-  return "Expenses";
+  return <span>Expenses</span>;
 }
--- app/routes/__auth.signup.tsx
+++ app/routes/__auth.signup.tsx
@@ -1,3 +1,3 @@
 export default function Signup() {
-    return "Signup";
-  }
+  return <span>Signup</span>;
+}
