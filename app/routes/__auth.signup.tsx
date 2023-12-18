1cf739c0ad3a0c27f05e1414be63d74c51459230
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2023-12-18 12:10:42 -0300

    create sign up page (wip)

--- app/routes/__auth.signup.tsx
+++ app/routes/__auth.signup.tsx
@@ -1,3 +1,26 @@
+import InputText from "~/components/inputText/InputText";
+
 export default function Signup() {
-  return <span>Signup</span>;
+  return (
+    <div className="h-screen bg-violet-950 flex justify-center items-center">
+      <div className="bg-white rounded-lg h-5/6 w-full mx-4 md:w-3/5 px-5">
+        <h1 className="text-center mt-3 text-2xl font-bold text-violet-950">
+          Sign Up
+        </h1>
+
+        <div>
+          <InputText label="Name"></InputText>
+          <InputText label="Login"></InputText>
+          <InputText label="Password" type="password"></InputText>
+          <InputText label="Repeat Password" type="password"></InputText>
+        </div>
+
+        <div className="text-right">
+          <button className="bg-violet-950 text-white rounded-lg px-10 py-1">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
