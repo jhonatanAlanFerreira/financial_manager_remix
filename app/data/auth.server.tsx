415b75917bfc91f34293e94ae31e79c409601996
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2024-01-08 20:32:30 -0300

    hash the user password

--- app/data/auth.server.tsx
+++ app/data/auth.server.tsx
@@ -2,6 +2,7 @@ import { prisma } from "~/data/database.server";
 import SignupRequest from "~/interfaces/bodyRequests/SignupRequest";
 import { signupValidator } from "~/data/requestValidators/authValidator";
 import ServerResponse from "~/interfaces/ServerResponse";
+import { hash } from "bcrypt";
 
 export async function signup(data: SignupRequest): Promise<ServerResponse> {
   const dataIsValid = signupValidator(data);
@@ -14,11 +15,13 @@ export async function signup(data: SignupRequest): Promise<ServerResponse> {
     };
   }
 
+  const passwordHash = await hash(data.password, 12);
+
   const user = await prisma.user.create({
     data: {
       name: data.name,
       login: data.login,
-      password: data.password,
+      password: passwordHash,
     },
   });
 
