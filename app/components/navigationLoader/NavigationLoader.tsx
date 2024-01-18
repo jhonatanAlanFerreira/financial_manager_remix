9a656219391f131ac5ffd66404fc036a521a3fce
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2024-01-17 22:56:11 -0300

    use loader in navigationLoader component

--- app/components/navigationLoader/NavigationLoader.tsx
+++ app/components/navigationLoader/NavigationLoader.tsx
@@ -1,5 +1,6 @@
 import { useNavigation } from "@remix-run/react";
 import { ReactNode } from "react";
+import Loader from "../loader/Loader";
 
 export default function NavigationLoader({
   children,
@@ -8,20 +9,5 @@ export default function NavigationLoader({
 }) {
   const navigation = useNavigation();
 
-  return (
-    <>
-      {navigation.state === "loading" && (
-        <div className="flex items-center justify-center h-screen w-screen z-10 absolute">
-          <div className="loader ease-linear border-t-4 border-violet-950 border-solid rounded-full animate-spin h-16 w-16"></div>
-        </div>
-      )}
-      <div
-        className={
-          navigation.state === "loading" ? "blur-sm pointer-events-none" : ""
-        }
-      >
-        {children}
-      </div>
-    </>
-  );
+  return <Loader loading={navigation.state === "loading"}>{children}</Loader>;
 }
