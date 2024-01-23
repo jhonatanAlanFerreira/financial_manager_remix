0fa8e53fc387667485e7dd1e538b8b9f626b9baf
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2024-01-22 21:33:31 -0300

    fix name from expense payload

--- app/routes/api/expense/index.tsx
+++ app/routes/api/expense/index.tsx
@@ -32,7 +32,7 @@ let createExpense = async (request: Request) => {
   const body = await request.formData();
 
   const data: ExpenseCreateRequest = {
-    name: String(body.get("name")),
+    name: String(body.get("name") || ""),
     amount: +(body.get("amount") || 0),
     is_personal_expense: !!body.get("is_personal_expense"),
   };
