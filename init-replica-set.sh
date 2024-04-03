8da4e814dfef5142f86677666c35e2236047daed
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2024-04-03 11:23:59 -0300

    configure docker-compose

---
+++ init-replica-set.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# init-replica-set.sh
+
+hosts=("mongo" "mongo_replica_1" "mongo_replica_2")
+
+# Initiate the replica set
+mongosh --host ${hosts[0]} --eval "
+rs.initiate({
+    _id: 'financialDbReplicaSet',
+    members: [
+        {_id: 0, host: '${hosts[0]}'},
+        {_id: 1, host: '${hosts[1]}'},
+        {_id: 2, host: '${hosts[2]}'}
+    ]
+})
+"
