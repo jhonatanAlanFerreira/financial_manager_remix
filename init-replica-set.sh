3da11d57c8addf279792c2e32bbf49fd16c759a4
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2024-04-10 19:01:27 -0300

    improve docker configuration

--- init-replica-set.sh
+++ init-replica-set.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 # init-replica-set.sh
 
-hosts=("mongo_financial_manager" "mongo_financial_manager_replica_1" "mongo_financial_manager_replica_2")
+wait_for_mongo() {
+    local host="$1"
+    echo "Waiting for MongoDB at $host to be ready..."
+    while true; do
+        if mongosh --host "$host" --eval "db.adminCommand('ping')" &>/dev/null; then
+            echo "MongoDB at $host is ready!"
+            break
+        else
+            echo "MongoDB at $host not ready, retrying in 2 seconds..."
+            sleep 2
+        fi
+    done
+}
 
-# Initiate the replica set
-mongosh --host ${hosts[0]} --eval "
+wait_for_mongo host.docker.internal:$MONGO_FINANCIAL_MANAGER_PORT
+
+mongosh --host host.docker.internal:$MONGO_FINANCIAL_MANAGER_PORT --eval "
 rs.initiate({
     _id: 'financialDbReplicaSet',
     members: [
-        {_id: 0, host: '${hosts[0]}'},
-        {_id: 1, host: '${hosts[1]}'},
-        {_id: 2, host: '${hosts[2]}'}
+        {_id: 0, host: 'host.docker.internal:$MONGO_FINANCIAL_MANAGER_PORT'},
+        {_id: 1, host: 'host.docker.internal:$MONGO_FINANCIAL_MANAGER_REPLICA_1_PORT'},
+        {_id: 2, host: 'host.docker.internal:$MONGO_FINANCIAL_MANAGER_REPLICA_2_PORT'}
     ]
 })
 "
