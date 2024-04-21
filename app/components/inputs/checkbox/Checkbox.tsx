9df99a7c21bca0fc40203070de989f4ea02e4118
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2024-04-21 15:34:35 -0300

    fix checkbox color

--- app/components/inputs/checkbox/Checkbox.tsx
+++ app/components/inputs/checkbox/Checkbox.tsx
@@ -7,7 +7,7 @@ export default function Checkbox({
     <input
       type="checkbox"
       {...props}
-      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+      className="accent-violet-950 w-4 h-4 bg-gray-100 border-gray-300 rounded focus:ring-violet-950 focus:ring-2"
     />
   );
 }
