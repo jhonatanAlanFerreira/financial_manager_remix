962a5232c281b7d686a654e0e227b692ae3ba727
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2024-07-12 17:41:47 -0300

    improve className property for button components

--- app/components/buttons/danger-button/DangerButton.tsx
+++ app/components/buttons/danger-button/DangerButton.tsx
@@ -4,15 +4,14 @@ import DangerButtonProps from "~/interfaces/componentsProps/DangerButtonProps";
 export default function DangerButton({
   text,
   iconName,
-  className,
   ...rest
 }: DangerButtonProps) {
-  const existingClasses =
-    "flex gap-3 justify-center text-white bg-red-700 py-2 px-5 m-2 rounded shadow shadow-gray-950 transform transition-transform duration-300 hover:scale-110";
-  const combinedClasses = `${existingClasses} ${className}`;
-
   return (
-    <button className={combinedClasses} style={{ minWidth: "7rem" }} {...rest}>
+    <button
+      {...rest}
+      className={`flex gap-3 justify-center text-white bg-red-700 py-2 px-5 m-2 rounded shadow shadow-gray-950 transform transition-transform duration-300 hover:scale-110 ${rest.className}`}
+      style={{ minWidth: "7rem" }}
+    >
       {text} {iconName && <Icon name={iconName}></Icon>}
     </button>
   );
--- app/components/buttons/primary-button/PrimaryButton.tsx
+++ app/components/buttons/primary-button/PrimaryButton.tsx
@@ -4,15 +4,14 @@ import PrimaryButtonProps from "~/interfaces/componentsProps/PrimaryButtonProps"
 export default function PrimaryButton({
   text,
   iconName,
-  className,
   ...rest
 }: PrimaryButtonProps) {
-  const existingClasses =
-    "flex justify-center gap-3 text-white bg-violet-950 py-2 px-5 m-2 rounded shadow shadow-gray-950 transform transition-transform duration-300 hover:scale-110";
-  const combinedClasses = `${existingClasses} ${className}`;
-
   return (
-    <button className={combinedClasses} style={{ minWidth: "7rem" }} {...rest}>
+    <button
+      {...rest}
+      className={`flex justify-center gap-3 text-white bg-violet-950 py-2 px-5 m-2 rounded shadow shadow-gray-950 transform transition-transform duration-300 hover:scale-110 ${rest.className}`}
+      style={{ minWidth: "7rem" }}
+    >
       {text} {iconName && <Icon name={iconName}></Icon>}
     </button>
   );
