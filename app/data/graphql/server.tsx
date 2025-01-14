a5b77cc3781019feea45cec6f88dab070e78eb24
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-01-14 20:36:50 -0300

    implement graphql WIP

---
+++ app/data/graphql/server.tsx
@@ -0,0 +1,30 @@
+import { createYoga } from "graphql-yoga";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { GraphQLSchema } from "graphql";
+
+const typeDefs = `
+  type Query {
+    test: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    test: () => "Working!",
+  },
+};
+
+const schema: GraphQLSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const yoga = createYoga({
+  schema,
+  context: ({ request }) => ({
+    request,
+  }),
+  graphiql: true,
+});
+
+export const graphqlHandler = yoga.handle;
--- app/routes/graphql/index.tsx
+++ app/routes/graphql/index.tsx
@@ -1,32 +1,9 @@
-import { useState } from "react";
-import { Sidebar } from "~/components/siderbar/sidebar";
-import { TopBar } from "~/components/top-bar/top-bar";
+import { graphqlHandler } from "~/data/graphql/server";
 
-export default function Graphql() {
-  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+export const loader = async ({ request }: { request: Request }) => {
+  return graphqlHandler(request);
+};
 
-  const updateSidebarOpen = (value: boolean) => {
-    setSidebarOpen(value);
-  };
-
-  return (
-    <div className="flex flex-col h-screen">
-      <TopBar
-        updateSidebarOpen={updateSidebarOpen}
-        overrideTitle={{ pageTitle: "GraphQL Playground" }}
-      ></TopBar>
-      <Sidebar
-        sidebarOpen={sidebarOpen}
-        updateSidebarOpen={updateSidebarOpen}
-      ></Sidebar>
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-50 text-gray-800">
-        <h1 className="text-3xl font-bold mb-4">GraphQL Playground</h1>
-        <p className="text-lg text-gray-600 text-center max-w-md">
-          This page will soon feature a fully functional GraphQL playground for
-          exploring and testing API queries and mutations.
-          <br />
-        </p>
-      </div>
-    </div>
-  );
-}
+export const action = async ({ request }: { request: Request }) => {
+  return graphqlHandler(request);
+};
