ee9661d49e0dc0009ae56360a2af6281d8a4d508
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-02-11 20:36:37 -0300

    create dataloader POC

--- app/data/graphql/schema/type-defs/classification/index.tsx
+++ app/data/graphql/schema/type-defs/classification/index.tsx
@@ -1,8 +1,52 @@
 import typeDefs from "~/data/graphql/schema/type-defs/classification/classification.graphql";
 import { SchemaDefInterface } from "~/data/graphql/schema/graphql-globals";
-import { listTransactionsByClassification } from "~/data/graphql/schema/resolvers/transaction/list-transactions-resolver";
+import { Transaction, TransactionClassification } from "@prisma/client";
+import { prisma } from "~/data/database/database.server";
+import DataLoader from "dataloader";
 
-const Classification = { transactions: listTransactionsByClassification };
+const transactionByClassificationBatch = async (
+  classifications: readonly TransactionClassification[]
+): Promise<Transaction[][]> => {
+  console.log("TRANSACTION LOADER");
+
+  const classificationIds = classifications.map((c) => c.id);
+
+  const transactions = await prisma.transaction.findMany({
+    where: {
+      transaction_classification_ids: {
+        hasSome: classificationIds,
+      },
+    },
+  });
+
+  const transactionMap: Record<string, Transaction[]> = {};
+  classificationIds.forEach((id) => {
+    transactionMap[id] = [];
+  });
+
+  transactions.forEach((tx) => {
+    tx.transaction_classification_ids.forEach((cid) => {
+      if (transactionMap[cid]) {
+        transactionMap[cid].push(tx);
+      }
+    });
+  });
+
+  return classificationIds.map((id) => {
+    const result = transactionMap[id];
+    return result;
+  });
+};
+
+const transactionLoader = new DataLoader(
+  (classifications: readonly TransactionClassification[]) =>
+    transactionByClassificationBatch(classifications)
+);
+
+const Classification = {
+  transactions: (classification: TransactionClassification) =>
+    transactionLoader.load(classification),
+};
 
 const schemaDef: SchemaDefInterface = {
   typeDefs,
