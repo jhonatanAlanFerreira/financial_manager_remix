838bc35840eabc52c000ca1f0586a11b03bea970
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-02-12 15:15:42 -0300

    remove unused line

--- app/data/graphql/schema/resolvers/transaction/list-transactions-resolver.tsx
+++ app/data/graphql/schema/resolvers/transaction/list-transactions-resolver.tsx
@@ -1,6 +1,5 @@
 import { requireUserSession } from "~/data/auth/auth.server";
 import { prisma } from "~/data/database/database.server";
-import { createLoaders } from "~/data/graphql/loaders";
 import { LoadersInterface } from "~/data/graphql/loaders/loaders-interface";
 import { buildTransactionWhereClause } from "~/data/graphql/query-builders/transactions-query-builders";
 import { ListTransactionResolverParamsInterface } from "~/data/graphql/schema/resolvers/transaction/list-transactions-resolver-interfaces";
