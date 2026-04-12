0051bc78139ec93432615773ebc5f490038dd354
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2026-04-12 10:34:06 -0300

    standardize graphql types

--- app/data/graphql/schema/query/index.ts
+++ app/data/graphql/schema/query/index.ts
@@ -4,17 +4,17 @@ import { SchemaDefInterface } from "~/data/graphql/schema/graphql-globals";
 import { listTransactions } from "~/data/graphql/schema/resolvers/transaction/list-transactions-resolver";
 import { listClassifications } from "~/data/graphql/schema/resolvers/classification/list-classification-resolver";
 
-export const Query = {
-  chartTransactionData: listChartTransactionData,
-  transactions: listTransactions,
-  classifications: listClassifications,
+export const resolvers = {
+  Query: {
+    chartTransactionData: listChartTransactionData,
+    transactions: listTransactions,
+    classifications: listClassifications,
+  },
 };
 
 const schemaDef: SchemaDefInterface = {
   typeDefs,
-  resolvers: {
-    Query,
-  },
+  resolvers,
 };
 
 export default schemaDef;
--- app/data/graphql/schema/type-defs/classification/index.ts
+++ app/data/graphql/schema/type-defs/classification/index.ts
@@ -2,13 +2,13 @@ import typeDefs from "~/data/graphql/schema/type-defs/classification/classificat
 import { SchemaDefInterface } from "~/data/graphql/schema/graphql-globals";
 import { listTransactionsByClassification } from "~/data/graphql/schema/resolvers/transaction/list-transactions-resolver";
 
-const Classification = { transactions: listTransactionsByClassification };
+const resolvers = {
+  Classification: { transactions: listTransactionsByClassification },
+};
 
 const schemaDef: SchemaDefInterface = {
   typeDefs,
-  resolvers: {
-    Classification,
-  },
+  resolvers,
 };
 
 export default schemaDef;
